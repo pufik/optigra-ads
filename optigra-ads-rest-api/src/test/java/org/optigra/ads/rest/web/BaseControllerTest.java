b8182dc3a30d815ae920a1acdba363afb68c39d0
Ivan Ursul <i.ursul@optigra.org>
2014-01-23 14:18:46 +0200

    Added content expected statement

--- optigra-ads-rest-api/src/test/java/org/optigra/ads/rest/web/BaseControllerTest.java
+++ optigra-ads-rest-api/src/test/java/org/optigra/ads/rest/web/BaseControllerTest.java
@@ -1,12 +1,15 @@
 package org.optigra.ads.rest.web;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.codehaus.jackson.map.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.optigra.ads.model.User;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
@@ -19,6 +22,7 @@ public class BaseControllerTest {
 	
     private MockMvc mockMvc;
     
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Before
     public void setup() {
@@ -27,8 +31,13 @@ public class BaseControllerTest {
 
     @Test
     public void testGetUser() throws Exception {
+        long userId = 10L;
+        User user = new User();
+        user.setId(userId);
+        
     	// When
-    	mockMvc.perform(get("/api"))
-    		.andExpect(status().isOk());
+        mockMvc.perform(get("/api"))
+    		.andExpect(status().isOk())
+    		.andExpect(content().string(objectMapper.writeValueAsString(user)));
     }
 }
